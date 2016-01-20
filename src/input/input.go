333a8a94b3f6d4f99b4a14ed6e68f6ebed32aa09
Mariusz Gronczewski <xani666@gmail.com>
2016-01-20 15:00:34 +0100

    dummy input plugin

--- src/input/input.go
+++ src/input/input.go
@@ -17,3 +17,61 @@ type Input interface {
 func New(inputName string, inputAddr string) {
 
 }
+
+
+type DummyIn struct{}
+
+func NewDummy(addr string, user string, pass string, db string) (Input, error) {
+	var dummy DummyIn
+	var err error
+	return &dummy,err
+}
+
+func (d *DummyIn) GetSeriesList() ([]string, error) {
+	var err error
+	return []string{
+		"dc1.host1.cpu.user",
+		"dc2.host2.cpu.idle",
+	}, err
+}
+
+func (d *DummyIn) GetFieldRangeByName(name string, start time.Time, end time.Time) ([]common.Field, error) {
+	var err error
+	var startField common.Field
+	var midField common.Field
+	var endField common.Field
+
+	startTs := start.UnixNano()
+	endTs := end.UnixNano()
+
+	if startTs - endTs < 10 || endTs - startTs < 10 {
+
+	}
+
+	startField.Name = name
+	startField.Tags = make(map[string]string)
+	startField.Values = make(map[string]interface{})
+	startField.Values[`time`] = startTs
+
+	// same TS, emit only one record
+	if startTs == endTs {
+		return []common.Field{startField}, err
+	}
+	endField.Name = name
+	endField.Tags = make(map[string]string)
+	endField.Values = make(map[string]interface{})
+	endField.Values[`time`] = endTs
+
+	// very small spread, emit only start and end
+	if startTs - endTs < time.Second.Nanoseconds() || endTs - startTs < time.Second.Nanoseconds() {
+		return []common.Field{startField, endField}, err
+	}
+
+	midField.Name = name
+	midField.Tags = make(map[string]string)
+	midField.Values = make(map[string]interface{})
+	midField.Values[`time`] = (startTs + endTs ) / 2
+
+
+	return []common.Field{startField, midField, endField}, err
+}
--- src/input/input_test.go
+++ src/input/input_test.go
@@ -1,8 +1,27 @@
 package input
 
 import (
-//	. "github.com/smartystreets/goconvey/convey"
-//	"testing"
-//	"time"
-//	"fmt"
+	. "github.com/smartystreets/goconvey/convey"
+	"testing"
+	"time"
 )
+
+
+func TestDummy(t *testing.T) {
+	// FIXME testdb with pregenerated data
+	i, err := NewDummy("http://localhost:8086", "root","root","stats")
+	series, err := i.GetSeriesList()
+	Convey("GetSeriesList", t, func() {
+		So(err, ShouldEqual, nil)
+		So(len(series), ShouldBeGreaterThan,0)
+	})
+	tsStart := time.Now().Add(-1 * time.Hour)
+	points, err := i.GetFieldRangeByName(series[0],tsStart,time.Now())
+	Convey("GetFirstPoint", t, func() {
+		So(err, ShouldEqual, nil)
+		So(points, ShouldNotEqual, nil)
+		So(points[0].Name, ShouldNotEqual,"")
+		So(points[0].Values["time"], ShouldBeGreaterThan,0)
+
+	})
+}
